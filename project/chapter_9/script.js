f506e76169a4c2eb7525c2514640bb83d3f888ad
Kai Prenger <kai.prenger@gmail.com>
2022-09-24 17:54:36 -0400

    set-up before interactivity

--- project/chapter_9/script.js
+++ project/chapter_9/script.js
@@ -1,1 +1,16 @@
 // alert("Everything's coming up Milhouse!");
+// $("h1").hide();
+$("#noShow").hide();
+$("h1").html("The Best Jokes Ever!")
+$("#joke").append("<p>What do you call a fake noodle? <br /> A. An impasta</p>");
+
+// modify ccs style for button
+$("#toggleJokes").css({
+    "background-color": "orange",
+    "color": "white",
+    "font-size": "20px",
+    "padding": "10px",
+    "border-radius": "5px",
+    "border": "2px solid black",
+    "margin": "10px"
+});
