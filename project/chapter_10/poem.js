63c2b10f16330ea65854def200eb7aee95bcaaa9
Kai Prenger <kai.prenger@gmail.com>
2022-10-02 18:11:58 -0400

    Getting info div to change

--- project/chapter_10/poem.js
+++ project/chapter_10/poem.js
@@ -1,15 +1,28 @@
-$('#info').html("<p>The info will go here</p>");
-
-let line1, line1Text;
-
-line1 = [{text: "What", info: "Anaphora: The repetition of a word or phrase at \
-    the beginning of (usually successive) lines. For example, the use of What \
-    in the first four lines."}
-    , {text: "hurrying"}, {text: "human"}, {text: "tides"},
+$("#info").html("<p>The info will go here.</p>");
+let line1, line1Text; // don’t need the intermediate step of line1TextArray
+line1 = [{text:  "What", info: "Anaphora: The repetition of a word or phrase at the beginning of (usually successive) lines. For example, the use of What in the first four lines."}, 
+    {text: "hurrying"}, {text: "human"}, {text: "tides"}, 
     {text: "or"}, {text: "day"}, {text: "or"}, {text: "night!"}];
-
-line1Text = line1.map(function(word){
-    return word.text;
-}).join(" ");
-
-$("#poem").html("<p>" + line1Text + "<br /></p>");
+// Create a blank string that opens two tags.
+line1Text = "<blockquote><p>"; // opening tags for blockquote and p, we close them in the loop below
+line1.map(function(word){
+  // Define a variable that will be the entirety of a single
+  // word-sized chunk of information.
+  let wordString;
+  wordString = word.text;
+  // Test to see if the .info property exists.
+  if (word.info){
+    // If it does, surround wordString in an <a> tag.
+    wordString = "<a href='#'>" + wordString + "</a>";
+  }
+  // Add wordString plus a space to the line1Text.
+  line1Text = line1Text + wordString + " ";
+});
+$("#poem").html(line1Text);
+$("#poem a").click(function(){
+    // Define the text and the word that was clicked.
+    let infoText, clickedWord;
+    clickedWord = $( this ).text();
+    infoText = "<h2>You clicked on the word: " + clickedWord + "</h2>";
+    $("#info").html(infoText);
+  });
