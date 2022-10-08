14d81d61887a2342347975b71bf8abb1d977bd24
Kai Prenger <kai.prenger@gmail.com>
2022-10-08 16:29:27 -0400

    End of chapter

--- project/chapter_10/poem.js
+++ project/chapter_10/poem.js
@@ -1,29 +1,32 @@
-$("#info").html("<p>The info will go here.</p>");
-let line1, line1Text; // don’t need the intermediate step of line1TextArray
-line1 = [{text:  "What", info: "Anaphora: The repetition of a word or phrase at the beginning of (usually successive) lines. For example, the use of What in the first four lines."}, 
-    {text: "hurrying"}, {text: "human"}, {text: "tides"}, 
-    {text: "or"}, {text: "day"}, {text: "or"}, {text: "night!"}];
-// Create a blank string that opens two tags.
-line1Text = "<blockquote><p>"; // opening tags for blockquote and p, we close them in the loop below
-line1.map(function(word){
-  // Define a variable that will be the entirety of a single
-  // word-sized chunk of information.
-  let wordString;
-  wordString = word.text;
-  // Test to see if the .info property exists.
-  if (word.info){
-    // If it does, surround wordString in an <a> tag.
-    wordString = "<a href='#' data-info='" + word.info + "'>" + wordString + "</a>";
-  }
-  // Add wordString plus a space to the line1Text.
-  line1Text = line1Text + wordString + " ";
-});
-$("#poem").html(line1Text);
-$("#poem a").click(function(){
-    // Define the text and the word that was clicked.
+// Step 1 - define the extra information for poem
+$("#info").html("<p>Extra info will go here.</p>");
+
+// Step 2 - display poem and allow clicking and info property display
+$.getJSON("poem.json", function(data){ // data is JSON object
+  let poemText; 
+  poemText = "<blockquote><p>"; 
+  //  map over data's .lines property
+  data.lines.map(function(line){ 
+    let lineText = "";
+    // map over each line.
+    line.map(function(word){
+      let wordString;
+      wordString = word.text;
+      if (word.info){
+        wordString = "<a href='#' data-info='" + word.info + "'>" + wordString + "</a>";
+      }
+      lineText = lineText + wordString + " "; // add the word to the lineText with a spacie
+    });
+    poemText = poemText + lineText + "<br/>"; // add lineText to poemText with a break
+  });
+  poemText = poemText + "</p></blockquote>";
+  $("#poem").html(poemText);
+ 
+  $("#poem a").click(function(){
     let infoText, clickedWord, clickedInfo;
     clickedWord = $( this ).text();
     clickedInfo = $( this ).data("info");
     infoText = clickedInfo;
     $("#info").html(infoText);
   });
+});
