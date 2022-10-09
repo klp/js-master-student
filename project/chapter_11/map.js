57bead96b87afdbb35a1617bd8cf899ae983804b
Kai Prenger <kai.prenger@gmail.com>
2022-10-09 17:04:57 -0400

    polyline between 111th street station and Queens Night Market

--- project/chapter_11/map.js
+++ project/chapter_11/map.js
@@ -29,3 +29,25 @@ const polyline = L.polyline([
 
 polyline.bindPopup("<b>Times Square to Graduate Center</b>");
 circle.bindPopup("I'm a circle!");
+
+// pan the map to times square
+poemMap.panTo(timesSquare.getLatLng());
+
+// Challenge
+// create markers for night market and 111 train station
+const queensNightMarket = L.marker([40.74826747961223, -73.85054568858276]).addTo(poemMap);
+const oneEleventhStreetStation = L.marker([40.75183557683284, -73.85534147827792]).addTo(poemMap);
+const latLongs = [
+    [40.75183557683284, -73.85534147827792],
+    [40.751583621809644, -73.85582427589705],
+    [40.74748718679847, -73.85351757615341],
+    [40.747308368413016, -73.85259489625905],
+    [40.74836501553473, -73.85294894784641],
+    [40.748568214979436, -73.85246615022729],
+    [40.74826747961223, -73.85054568858276]
+];
+
+// draw polyline from station to night market
+const nightMarketPolyline = L.polyline(latLongs, {color: 'green'}).addTo(poemMap);
+// Show both points in default view of maps
+poemMap.fitBounds(nightMarketPolyline.getBounds());
