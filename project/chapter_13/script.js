fd0339ac708eee03ac26628f22ecb1678db1d911
klp <kai.prenger@gmail.com>
2022-11-17 13:32:20 -0500

    some clean up and attempting to output js and jquery stuff on the page

--- project/chapter_13/script.js
+++ project/chapter_13/script.js
@@ -16,11 +16,15 @@ const dogElement = sentence.indexOf('dog');
 
 console.log(dogElement);
 
+$("#dog-sentence").append("The index of the substring 'dog' in '" + sentence + "' is " + dogElement);
+
 const someArray = [40, 38, 37, 93, 55, 66];
 
 const filteredArray = someArray.filter(function(value) {
     return value > 50;
-})
+});
+
+$("#filtered-array").append(`If I filter values less than 50 from ${someArray} I get an array of [${filteredArray}]`);
 
 console.log(filteredArray);
 
@@ -29,5 +33,7 @@ console.log(Math.ceil(0.36));
 function generateRandom (min, max) {
     return Math.random() * (max - min) + min;
 }
+
+$("#random-num").append(`A random number between 0 & 100: ${generateRandom(0, 100)}`);
 
 console.log(generateRandom(0, 101));
